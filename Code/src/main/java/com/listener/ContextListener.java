06c14f6f3c3dba1b9ff9c0a4de362b9d2315b1be
vignesh <118158912+viki-kali@users.noreply.github.com>
2025-03-20 17:17:33 +0530

    Update ContextListener.java

--- Code/src/main/java/com/listener/ContextListener.java
+++ Code/src/main/java/com/listener/ContextListener.java
@@ -10,7 +10,7 @@ public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         // Set Derby's system home using an absolute path.
-        System.setProperty("derby.system.home", "D:\\Users\\2788364\\Eclipse\\Final CaseStudy\\Airline Management System");
+        System.setProperty("derby.system.home", "D:\\Airline Management System");
         System.out.println("Derby system home set to: " + System.getProperty("derby.system.home"));
     }
 
